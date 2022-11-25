b93fe016daa0798cbe4f8565a366a1863e9a2e4d
Mattia Moretti <mattia.moretti@oracle.com>
2022-11-25 10:00:35 +0100

    boh

--- pkg/datasource.go
+++ pkg/datasource.go
@@ -3,11 +3,12 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 	"regexp"
 	"sort"
 	"strconv"
@@ -101,6 +102,8 @@ type GrafanaCommonRequest struct {
 // Query - Determine what kind of query we're making
 func (o *OCIDatasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
 	var ts GrafanaCommonRequest
+	var p fileConfigurationProvider
+
 	var takey string
 
 	query := req.Queries[0]
@@ -149,7 +152,7 @@ func (o *OCIDatasource) QueryData(ctx context.Context, req *backend.QueryDataReq
 	case "regions":
 		return o.regionsResponse(ctx, req, takey)
 	case "tenancies":
-		return o.tenancyConfigResponse(ctx, req)
+		return p.tenancyConfigResponse(ctx, req)
 	case "search":
 		return o.searchResponse(ctx, req, takey)
 	case "test":
@@ -161,6 +164,8 @@ func (o *OCIDatasource) QueryData(ctx context.Context, req *backend.QueryDataReq
 
 func (o *OCIDatasource) testResponse(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
 	var ts GrafanaCommonRequest
+	var p fileConfigurationProvider
+
 	var tenancyocid string
 	var tenancyErr error
 
@@ -169,7 +174,7 @@ func (o *OCIDatasource) testResponse(ctx context.Context, req *backend.QueryData
 		return &backend.QueryDataResponse{}, err
 	}
 
-	regions, _ := OCIConfigParser("regions")
+	regions, _ := p.OCIConfigParser("regions")
 	rr := 0
 	reg := common.StringToRegion(ts.Region)
 
@@ -298,6 +303,7 @@ func (o *OCIDatasource) resourcegroupsResponse(ctx context.Context, req *backend
 }
 
 func (o *OCIDatasource) getConfigProvider(environment string, tenancymode string) error {
+	var p fileConfigurationProvider
 
 	o.logger.Debug("getConfigProvider")
 	o.logger.Debug(environment)
@@ -305,7 +311,7 @@ func (o *OCIDatasource) getConfigProvider(environment string, tenancymode string
 	switch environment {
 	case "local":
 		if tenancymode == "multitenancy" {
-			ociconfigs, _ := OCIConfigParser("ociconfigs")
+			ociconfigs, _ := p.OCIConfigParser("ociconfigs")
 			for _, ociconfig := range ociconfigs {
 				var configProvider common.ConfigurationProvider
 				configProvider = common.CustomProfileConfigProvider("", ociconfig)
@@ -831,9 +837,9 @@ Function generates an array  containing OCI configuration (.oci/config) in the f
 
 */
 
-func (o *OCIDatasource) tenancyConfigResponse(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
+func (p fileConfigurationProvider) tenancyConfigResponse(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
 	resp := backend.NewQueryDataResponse()
-	ociconfigs, _ := OCIConfigParser("ociconfigs")
+	ociconfigs, _ := p.OCIConfigParser("ociconfigs")
 
 	for _, query := range req.Queries {
 		frame := data.NewFrame(query.RefID, data.NewField("text", nil, []string{}))
@@ -861,7 +867,7 @@ It accepts one parameter (scope) which can be "ociconfigs" or "regions"
 if ociconfigs, then the function returns an array of the OCI config sections labels
 if regions, then the function returns the list of the regions of every OCI config section entries
 */
-func OCIConfigParser(scope string) ([]string, error) {
+func (p fileConfigurationProvider) OCIConfigParser(scope string) ([]string, error) {
 	var oci_config_file string
 	var ociconfigs []string
 	var regions []string
@@ -872,30 +878,11 @@ func OCIConfigParser(scope string) ([]string, error) {
 	}
 
 	if _, ok := os.LookupEnv("OCI_CLI_CONFIG_FILE"); ok {
-		oci_config_file = os.Getenv("OCI_CLI_CONFIG_FILE")
+		p.ConfigPath = os.Getenv("OCI_CLI_CONFIG_FILE")
 	} else {
-		oci_config_file = homedir + "/.oci/config"
+		p.ConfigPath = homedir + "/.oci/config"
 	}
-
-	file, err := os.Open(oci_config_file)
-	if err != nil {
-		return nil, errors.Wrap(err, "error opening file:"+oci_config_file)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	if err := scanner.Err(); err != nil {
-		return nil, errors.Wrap(err, "buffer error")
-	}
-
-	r, err := regexp.Compile(`\[.*\]`) // this can also be a regex
-	if err != nil {
-		return nil, errors.Wrap(err, "error in compiling regex")
-	}
-	r2, err := regexp.Compile(`region=`) // this can also be a regex
-	if err != nil {
-		return nil, errors.Wrap(err, "error in compiling regex")
-	}
+	p.readAndParseConfigFile()
 
 	if scope == "ociconfigs" {
 		for scanner.Scan() {
@@ -920,4 +907,390 @@ func OCIConfigParser(scope string) ([]string, error) {
 		return regions, nil
 	}
 	return nil, nil
+}
+
+// AuthenticationType for auth
+type AuthenticationType string
+
+const (
+	// UserPrincipal is default auth type
+	UserPrincipal AuthenticationType = "user_principal"
+	// InstancePrincipal is used for instance principle auth type
+	InstancePrincipal AuthenticationType = "instance_principal"
+	// InstancePrincipalDelegationToken is used for instance principle delegation token auth type
+	InstancePrincipalDelegationToken AuthenticationType = "instance_principle_delegation_token"
+	// UnknownAuthenticationType is used for none meaningful auth type
+	UnknownAuthenticationType AuthenticationType = "unknown_auth_type"
+)
+
+// AuthConfig is used for getting auth related paras in config file
+type AuthConfig struct {
+	AuthType AuthenticationType
+	// IsFromConfigFile is used to point out if the authConfig is from configuration file
+	IsFromConfigFile bool
+	OboToken         *string
+}
+
+// ConfigurationProvider wraps information about the account owner
+type ConfigurationProvider interface {
+	TenancyOCID() (string, error)
+	UserOCID() (string, error)
+	KeyFingerprint() (string, error)
+	Region() (string, error)
+}
+
+// rawConfigurationProvider allows a user to simply construct a configuration provider from raw values.
+type rawConfigurationProvider struct {
+	tenancy              string
+	user                 string
+	region               string
+	fingerprint          string
+	privateKey           string
+	privateKeyPassphrase *string
+}
+
+// NewRawConfigurationProvider will create a ConfigurationProvider with the arguments of the function
+func NewRawConfigurationProvider(tenancy, user, region, fingerprint, privateKey string, privateKeyPassphrase *string) ConfigurationProvider {
+	return rawConfigurationProvider{tenancy, user, region, fingerprint, privateKey, privateKeyPassphrase}
+}
+
+func (p rawConfigurationProvider) KeyID() (keyID string, err error) {
+	tenancy, err := p.TenancyOCID()
+	if err != nil {
+		return
+	}
+
+	user, err := p.UserOCID()
+	if err != nil {
+		return
+	}
+
+	fingerprint, err := p.KeyFingerprint()
+	if err != nil {
+		return
+	}
+
+	return fmt.Sprintf("%s/%s/%s", tenancy, user, fingerprint), nil
+}
+
+func (p rawConfigurationProvider) TenancyOCID() (string, error) {
+	if p.tenancy == "" {
+		return "", fmt.Errorf("tenancy OCID can not be empty")
+	}
+	return p.tenancy, nil
+}
+
+func (p rawConfigurationProvider) UserOCID() (string, error) {
+	if p.user == "" {
+		return "", fmt.Errorf("user OCID can not be empty")
+	}
+	return p.user, nil
+}
+
+func (p rawConfigurationProvider) KeyFingerprint() (string, error) {
+	if p.fingerprint == "" {
+		return "", fmt.Errorf("fingerprint can not be empty")
+	}
+	return p.fingerprint, nil
+}
+
+func (p rawConfigurationProvider) Region() (string, error) {
+	return p.region, nil
+}
+
+func (p rawConfigurationProvider) AuthType() (AuthConfig, error) {
+	return AuthConfig{UnknownAuthenticationType, false, nil}, nil
+}
+
+// fileConfigurationProvider. reads configuration information from a file
+type fileConfigurationProvider struct {
+	//The path to the configuration file
+	ConfigPath string
+
+	//The password for the private key
+	PrivateKeyPassword string
+
+	//The profile for the configuration
+	Profile string
+
+	//ConfigFileInfo
+	FileInfo *configFileInfo
+}
+
+type fileConfigurationProviderError struct {
+	err error
+}
+
+func (fpe fileConfigurationProviderError) Error() string {
+	return fmt.Sprintf("%s\nFor more info about config file and how to get required information, see https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm", fpe.err)
+}
+
+// ConfigurationProviderFromFileWithProfile creates a configuration provider from a configuration file
+// and the given profile
+func ConfigurationProviderFromFileWithProfile(configFilePath, profile, privateKeyPassword string) (fileConfigurationProvider, error) {
+	if configFilePath == "" {
+		return fileConfigurationProvider{}, fileConfigurationProviderError{err: fmt.Errorf("config file path can not be empty")}
+	}
+
+	return fileConfigurationProvider{
+		ConfigPath:         configFilePath,
+		PrivateKeyPassword: privateKeyPassword,
+		Profile:            profile}, nil
+}
+
+type configFileInfo struct {
+	UserOcid, Fingerprint, KeyFilePath, TenancyOcid, Region, Passphrase, SecurityTokenFilePath, DelegationTokenFilePath,
+	AuthenticationType string
+	PresentConfiguration rune
+}
+
+const (
+	hasTenancy = 1 << iota
+	hasUser
+	hasFingerprint
+	hasRegion
+	hasKeyFile
+	hasPassphrase
+	hasSecurityTokenFile
+	hasDelegationTokenFile
+	hasAuthenticationType
+	none
+)
+
+var profileRegex = regexp.MustCompile(`^\[(.*)\]`)
+
+func parseConfigFile(data []byte, profile string) (info *configFileInfo, err error) {
+
+	if len(data) == 0 {
+		return nil, fileConfigurationProviderError{err: fmt.Errorf("configuration file content is empty")}
+	}
+
+	content := string(data)
+	splitContent := strings.Split(content, "\n")
+
+	//Look for profile
+	for i, line := range splitContent {
+		if match := profileRegex.FindStringSubmatch(line); match != nil && len(match) > 1 && match[1] == profile {
+			start := i + 1
+			return parseConfigAtLine(start, splitContent)
+		}
+	}
+
+	return nil, fileConfigurationProviderError{err: fmt.Errorf("configuration file did not contain profile: %s", profile)}
+}
+
+func parseConfigAtLine(start int, content []string) (info *configFileInfo, err error) {
+	var configurationPresent rune
+	info = &configFileInfo{}
+	for i := start; i < len(content); i++ {
+		line := content[i]
+		if profileRegex.MatchString(line) {
+			break
+		}
+
+		if !strings.Contains(line, "=") {
+			continue
+		}
+
+		splits := strings.Split(line, "=")
+		switch key, value := strings.TrimSpace(splits[0]), strings.TrimSpace(splits[1]); strings.ToLower(key) {
+		case "passphrase", "pass_phrase":
+			configurationPresent = configurationPresent | hasPassphrase
+			info.Passphrase = value
+		case "user":
+			configurationPresent = configurationPresent | hasUser
+			info.UserOcid = value
+		case "fingerprint":
+			configurationPresent = configurationPresent | hasFingerprint
+			info.Fingerprint = value
+		case "key_file":
+			configurationPresent = configurationPresent | hasKeyFile
+			info.KeyFilePath = value
+		case "tenancy":
+			configurationPresent = configurationPresent | hasTenancy
+			info.TenancyOcid = value
+		case "region":
+			configurationPresent = configurationPresent | hasRegion
+			info.Region = value
+		}
+	}
+	info.PresentConfiguration = configurationPresent
+	return
+
+}
+
+// cleans and expands the path if it contains a tilde , returns the expanded path or the input path as is if not expansion
+// was performed
+func expandPath(filepath string) (expandedPath string) {
+	cleanedPath := path.Clean(filepath)
+	expandedPath = cleanedPath
+	if strings.HasPrefix(cleanedPath, "~") {
+		rest := cleanedPath[2:]
+		homedir, err := os.UserHomeDir()
+		if err != nil {
+			log.DefaultLogger.Error("could not get home directory")
+		}
+		expandedPath = path.Join(homedir, rest)
+	}
+	return
+}
+
+func openConfigFile(configFilePath string) (data []byte, err error) {
+	expandedPath := expandPath(configFilePath)
+	data, err = ioutil.ReadFile(expandedPath)
+	if err != nil {
+		err = fmt.Errorf("can not read config file: %s due to: %s", configFilePath, err.Error())
+		return nil, err
+	}
+	return data, nil
+}
+
+func (p fileConfigurationProvider) String() string {
+	return fmt.Sprintf("Configuration provided by file: %s", p.ConfigPath)
+}
+
+func (p fileConfigurationProvider) readAndParseConfigFile() (info *configFileInfo, err error) {
+	if p.FileInfo != nil {
+		return p.FileInfo, nil
+	}
+
+	if p.ConfigPath == "" {
+		return nil, fileConfigurationProviderError{err: fmt.Errorf("configuration path can not be empty")}
+	}
+
+	data, err := openConfigFile(p.ConfigPath)
+	if err != nil {
+		err = fileConfigurationProviderError{err: fmt.Errorf("error while parsing config file: %s. Due to: %s", p.ConfigPath, err.Error())}
+		return
+	}
+
+	p.FileInfo, err = parseConfigFile(data, p.Profile)
+	return p.FileInfo, err
+}
+
+func presentOrError(value string, expectedConf, presentConf rune, confMissing string) (string, error) {
+	if presentConf&expectedConf == expectedConf {
+		return value, nil
+	}
+	return "", fileConfigurationProviderError{err: errors.New(confMissing + " configuration is missing from file")}
+}
+
+func (p fileConfigurationProvider) TenancyOCID() (value string, err error) {
+	info, err := p.readAndParseConfigFile()
+	if err != nil {
+		err = fileConfigurationProviderError{err: fmt.Errorf("can not read tenancy configuration due to: %s", err.Error())}
+		return
+	}
+
+	value, err = presentOrError(info.TenancyOcid, hasTenancy, info.PresentConfiguration, "tenancy")
+	if err == nil && value == "" {
+		err = fileConfigurationProviderError{err: fmt.Errorf("tenancy OCID can not be empty when reading from config file")}
+	}
+	return
+}
+
+func (p fileConfigurationProvider) UserOCID() (value string, err error) {
+	info, err := p.readAndParseConfigFile()
+	if err != nil {
+		err = fileConfigurationProviderError{err: fmt.Errorf("can not read tenancy configuration due to: %s", err.Error())}
+		return
+	}
+
+	if value, err = presentOrError(info.UserOcid, hasUser, info.PresentConfiguration, "user"); err != nil {
+		// need to check if securityTokenPath is provided, if security token is provided, userOCID can be "".
+		if _, stErr := presentOrError(info.SecurityTokenFilePath, hasSecurityTokenFile, info.PresentConfiguration,
+			"securityTokenPath"); stErr == nil {
+			err = nil
+		}
+	}
+	return
+}
+
+func (p fileConfigurationProvider) KeyFingerprint() (value string, err error) {
+	info, err := p.readAndParseConfigFile()
+	if err != nil {
+		err = fileConfigurationProviderError{err: fmt.Errorf("can not read tenancy configuration due to: %s", err.Error())}
+		return
+	}
+	value, err = presentOrError(info.Fingerprint, hasFingerprint, info.PresentConfiguration, "fingerprint")
+	if err == nil && value == "" {
+		return "", fmt.Errorf("fingerprint can not be empty when reading from config file")
+	}
+	return
+}
+
+func (p fileConfigurationProvider) Region() (value string, err error) {
+	info, err := p.readAndParseConfigFile()
+	if err != nil {
+		err = fileConfigurationProviderError{err: fmt.Errorf("can not read region configuration due to: %s", err.Error())}
+		return
+	}
+
+	value, err = presentOrError(info.Region, hasRegion, info.PresentConfiguration, "region")
+	if err != nil {
+		return
+	}
+
+	return value, nil
+}
+
+// A configuration provider that look for information in  multiple configuration providers
+type composingConfigurationProvider struct {
+	Providers []ConfigurationProvider
+}
+
+// ComposingConfigurationProvider creates a composing configuration provider with the given slice of configuration providers
+// A composing provider will return the configuration of the first provider that has the required property
+// if no provider has the property it will return an error.
+func ComposingConfigurationProvider(providers []ConfigurationProvider) (ConfigurationProvider, error) {
+	if len(providers) == 0 {
+		return nil, fmt.Errorf("providers can not be an empty slice")
+	}
+
+	for i, p := range providers {
+		if p == nil {
+			return nil, fmt.Errorf("provider in position: %d is nil. ComposingConfiurationProvider does not support nil values", i)
+		}
+	}
+	return composingConfigurationProvider{Providers: providers}, nil
+}
+
+func (c composingConfigurationProvider) TenancyOCID() (string, error) {
+	for _, p := range c.Providers {
+		val, err := p.TenancyOCID()
+		if err == nil {
+			return val, nil
+		}
+	}
+	return "", fmt.Errorf("did not find a proper configuration for tenancy")
+}
+
+func (c composingConfigurationProvider) UserOCID() (string, error) {
+	for _, p := range c.Providers {
+		val, err := p.UserOCID()
+		if err == nil {
+			return val, nil
+		}
+	}
+	return "", fmt.Errorf("did not find a proper configuration for user")
+}
+
+func (c composingConfigurationProvider) KeyFingerprint() (string, error) {
+	for _, p := range c.Providers {
+		val, err := p.KeyFingerprint()
+		if err == nil {
+			return val, nil
+		}
+	}
+	return "", fmt.Errorf("did not find a proper configuration for keyFingerprint")
+}
+func (c composingConfigurationProvider) Region() (string, error) {
+	for _, p := range c.Providers {
+		val, err := p.Region()
+		if err == nil {
+			return val, nil
+		}
+	}
+
+	return "", fmt.Errorf("did not find a proper configuration for region, nor for OCI_REGION env var")
 }
