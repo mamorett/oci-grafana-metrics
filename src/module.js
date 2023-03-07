abf26d58382c9bfb6efbf48d7107169f5259a78a
Mattia Moretti <mattia.moretti@oracle.com>
2023-03-07 16:02:58 +0100

    fix2

--- src/module.js
+++ src/module.js
@@ -2,20 +2,15 @@
 ** Copyright © 2022 Oracle and/or its affiliates. All rights reserved.
 ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 */
-import OCIDatasource from './datasource'
-import { OCIDatasourceQueryCtrl } from './query_ctrl'
-import { OCIConfigCtrl } from './config_ctrl'
+import OCIDatasource from './datasource';
+import { OCIDatasourceQueryCtrl } from './query_ctrl';
+import { OCIConfigCtrl } from './config_ctrl';
 
 class OCIQueryOptionsCtrl {}
-OCIQueryOptionsCtrl.templateUrl = 'partials/query.options.html'
+OCIQueryOptionsCtrl.templateUrl = 'partials/query.options.html';
 
 class OCIAnnotationsQueryCtrl {}
-OCIAnnotationsQueryCtrl.templateUrl = 'partials/annotations.editor.html'
+OCIAnnotationsQueryCtrl.templateUrl = 'partials/annotations.editor.html';
 
-export {
-  OCIDatasource as Datasource,
-  OCIDatasourceQueryCtrl as QueryCtrl,
-  OCIConfigCtrl as ConfigCtrl,
-  OCIQueryOptionsCtrl as QueryOptionsCtrl,
-  OCIAnnotationsQueryCtrl as AnnotationsQueryCtrl
-}
+export { OCIDatasource as Datasource, OCIDatasourceQueryCtrl as QueryCtrl, OCIConfigCtrl as ConfigCtrl, OCIQueryOptionsCtrl as QueryOptionsCtrl, OCIAnnotationsQueryCtrl as AnnotationsQueryCtrl };
+//# sourceMappingURL=module.js.map
